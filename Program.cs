abb59ddedc5763287fcc57471c74d6854214193d
Felicia Müller <felicia-muller@hotmail.com>
2023-09-22 16:29:30 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿namespace ChessBoard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Skriv in en siffra");
+            int number = Int32.Parse(Console.ReadLine());
+
+            //Code to show the squares
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            string black = "◼︎";
+            string white = "◻︎";
+            
+
+            //Outer loop to run the inner loop
+            for (int outerLoop = 0; outerLoop < number; outerLoop++)
+
+            {
+
+                //The inner loop prints white and black squares depending on the conditions
+                for (int innerLoop = 0; innerLoop < number; innerLoop++)
+                {
+
+                    //Check conditions
+                    if (outerLoop % 2 == innerLoop % 2)
+                    {
+                        Console.Write(black);
+                    }
+            
+                    else
+                    {
+                        Console.Write(white);
+                    }
+      
+
+                }
+
+                //Empty line to start a new row
+                Console.WriteLine();
+                    
+            }
+
+           
+
+
+
+
+
+
+
+        }
+    }
+}
